5686608df901a441227e802ac3d52f64b53804c5
xiedong <770714526@qq.com>
2021-02-01 18:08:05 +0800

    增加.gitignore文件

--- XDTAF/XDTAF.h
+++ XDTAF/XDTAF.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT const unsigned char XDTAFVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XDTAF/PublicHeader.h>
 
-
